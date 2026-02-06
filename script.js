217dd8b9a36b9e2ec46174c67ba7863b046134da
khanhdocnope <khanh4346k9@gmail.com>
2026-02-06 14:31:40 +0700

    changes

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const dot = document.getElementById('cursor-dot');
-    const outline = document.getElementById('cursor-outline');
+document.addEventListener("DOMContentLoaded", () => {
+  const dot = document.getElementById("cursor-dot");
+  const outline = document.getElementById("cursor-outline");
 
-    if (!dot || !outline) return;
+  if (!dot || !outline) return;
 
-    window.addEventListener('mousemove', (e) => {
-        const posX = e.clientX;
-        const posY = e.clientY;
-        // Hiện cursor khi bắt đầu di chuyển
-        dot.style.opacity = "1";
-        outline.style.opacity = "1";
+  window.addEventListener("mousemove", (e) => {
+    const posX = e.clientX;
+    const posY = e.clientY;
 
+    dot.style.opacity = "1";
+    outline.style.opacity = "1";
 
-
-        // Outline: Di chuyển mượt mà (trễ hơn 1 chút)
-        outline.animate({
-            transform: `translate(${posX - 20}px, ${posY - 20}px)`
-        }, { duration: 500, fill: "forwards" });
-    });
+    outline.animate(
+      {
+        transform: `translate(${posX - 20}px, ${posY - 20}px)`,
+      },
+      { duration: 500, fill: "forwards" },
+    );
+  });
 });
