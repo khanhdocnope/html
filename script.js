a30390ed947837630eec336f0d0c1dc373ee3abc
khanhdocnope <khanh4346k9@gmail.com>
2026-02-06 13:40:30 +0700

    changes
    Remove comments in Vietnamese and adjust cursor visibility.

--- script.js
+++ script.js
@@ -7,13 +7,12 @@ document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('mousemove', (e) => {
         const posX = e.clientX;
         const posY = e.clientY;
-        // Hiện cursor khi bắt đầu di chuyển
+        
         dot.style.opacity = "1";
         outline.style.opacity = "1";
 
 
 
-        // Outline: Di chuyển mượt mà (trễ hơn 1 chút)
         outline.animate({
             transform: `translate(${posX - 20}px, ${posY - 20}px)`
         }, { duration: 500, fill: "forwards" });
